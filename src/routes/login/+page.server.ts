4626e04705bd9f5932f260cbfa1e5f4633731da6
Laxmanan Krishnapillai <65386461+Laxmanan-Krishnapillai@users.noreply.github.com>
2023-02-14 15:13:56 +0100

    try again

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -42,11 +42,11 @@ export const actions = {
 		const { lectioId, sessionId, expires, lectioTicket, lectiogsc } = client;
 		console.log(lectioId, sessionId, expires, lectioTicket, lectiogsc);
 		console.log(
-			`UPDATE (select id from student where lectio_id == ${lectioId} limit 1) MERGE {username: ${username}, password: ${password}, lectio_tokens: {token: ${sessionId}, expires: ${expires}, ticket: ${lectioTicket}, gsc: ${lectiogsc}}}`
+			`UPDATE (select id from student where lectio_id == "${lectioId}" limit 1) MERGE {username: "${username}", password: "${password}", lectio_tokens: {token: "${sessionId}", expires: "${expires}", ticket: "${lectioTicket}", gsc: "${lectiogsc}"}}`
 		);
 		const user = await cirql.execute({
 			query: query(
-				`UPDATE (select id from student where lectio_id == ${lectioId} limit 1) MERGE {username: ${username}, password: ${password}, lectio_tokens: {token: ${sessionId}, expires: ${expires}, ticket: ${lectioTicket}, gsc: ${lectiogsc}}}`
+				`UPDATE (select id from student where lectio_id == "${lectioId}" limit 1) MERGE {username: "${username}", password: "${password}", lectio_tokens: {token: "${sessionId}", expires: "${expires}", ticket: "${lectioTicket}", gsc: "${lectiogsc}"}}`
 			).single(),
 			schema: z.any()
 		});
